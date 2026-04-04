fe43682e1465ac4f4cb34ad90985f56b5bcb9821
k.driga <k.driga@skbkontur.ru>
2026-04-04 14:30:25 +0700

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+function dropConsole() {
+  return {
+    name: 'drop-console',
+    transform(code) {
+      if (process.env.NODE_ENV === 'production') {
+        return code.replace(/console\.(log|info|debug|warn|error)/g, '// console removed')
+      }
+      return code
+    }
+  }
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dropConsole()],
   base: './',
   build: {
     outDir: 'docs',
     assetsDir: 'assets',
+    minify: 'esbuild',
   },
 })
